3ba4209d62c0058e042fa6fbb52525e0916d02ad
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-12-01 11:49:08 +0100

    aggiunta abbassa e alza volume

--- src/multimedia/Main.java
+++ src/multimedia/Main.java
@@ -2,6 +2,8 @@ package multimedia;
 
 public class Main {
     public static void main(String[] args) {
-
+       Player pl=new Player();
+       pl.abbassaVolume(30);
+       pl.alzaVolume(51);
     }
 }
--- src/multimedia/Player.java
+++ src/multimedia/Player.java
@@ -2,5 +2,36 @@ package multimedia;
 
 public class Player {
 
-    
+    public int audio=50;
+    public int durata;
+
+
+    public int play(int audio,int durata){
+        this.audio=audio;
+        this.durata=durata;
+        return audio;
+    }
+
+
+    public int alzaVolume(int audio){
+        this.audio=audio;
+        if (audio!=50){
+            ++audio;
+           System.out.println(audio);
+        }else {
+           System.out.println("il volume è 50 di default");
+        }
+        return audio;
+    }
+
+    public int abbassaVolume(int audio){
+        this.audio=audio;
+        if (audio!=50){
+            --audio;
+            System.out.println(audio);
+        }else {
+            System.out.println("il volume è 50 di default");
+        }
+        return audio;
+    }
 }
